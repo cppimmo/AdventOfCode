2f1c01550031a7b71be90041a1fc92a4cfa93e80
Brian Hoffpauir <bhoffpauir0424@gmail.com>
2024-11-21 21:14:50 -0600

    Update header in AOC24

--- 2024/Source/AdventOfCodeStd.hpp
+++ 2024/Source/AdventOfCodeStd.hpp
@@ -1,15 +1,74 @@
 #ifndef ADVENTOFCODESTD_HPP
 #define ADVENTOFCODESTD_HPP
 
-#include <cstdlib>
-#include <cstdio>
-#include <cmath>
-#include <iostream>
-#include <string>
-#include <vector>
-#include <algorithm>
-#include <functional>
-#include <utility>
-#include <ranges>
+#include <bits/stdc++.h>
+/*
+ * Use:
+ * using namespace AOC;
+ */
+namespace AOC
+{
+	using ll = long long;               // Common shorthand for long long
+	using ull = unsigned long long;     // Unsigned long long
+	using ld = long double;             // Long double
+	using vi = std::vector<int>;        // Vector of integers
+	using vll = std::vector<long long>; // Vector of long longs
+	using vd = std::vector<double>;     // Vector of doubles
+	using vs = std::vector<std::string>; // Vector of strings
+	using pii = std::pair<int, int>;    // Pair of integers
+	using pll = std::pair<long long, long long>; // Pair of long longs
+	using vpii = std::vector<pii>;      // Vector of pairs of integers
+	using vpll = std::vector<pll>;      // Vector of pairs of long longs
+	using si = std::set<int>;           // Set of integers
+	using mii = std::map<int, int>;     // Map of integers to integers
+	using umii = std::unordered_map<int, int>; // Unordered map of integers
+
+	using Grid = std::vector<std::vector<int>>;  // 2D grid of integers
+	using Matrix = std::vector<std::vector<long long>>; // 2D grid of long longs
+
+	namespace rng = std::ranges;                 // Alias for ranges namespace
+	using UnaryFunc = std::function<int(int)>;   // Unary function taking and returning int
+	using BinaryFunc = std::function<int(int, int)>; // Binary function taking two ints
+
+	using MinHeap = std::priority_queue<int, std::vector<int>, std::greater<int>>; // Min-heap
+	using MaxHeap = std::priority_queue<int>; // Max-heap
+
+	using Bitset = std::bitset<32>; // Fixed-size bitset of 32 bits
+
+	using isstream = std::istringstream;  // Input string stream
+	using osstream = std::ostringstream; // Output string stream
+	using sstream = std::stringstream;        // General string stream
+
+	using Clock = std::chrono::high_resolution_clock; 
+	using TimePoint = std::chrono::time_point<Clock>;
+	using Duration = std::chrono::duration<double>; // General duration
+
+	using umap = std::unordered_map<int, int>; // Shorthand for unordered map
+	using uset = std::unordered_set<int>;     // Shorthand for unordered set
+	using deque = std::deque<int>;            // Shorthand for deque of integers
+	
+	using Index = std::size_t;                // Common alias for index types
+	using Coord = std::pair<int, int>;        // For 2D grid coordinates
+	using Path = std::vector<Coord>;          // Sequence of coordinates
+
+	class Timer
+	{
+	private:
+		std::string m_name;
+		TimePoint m_start;
+	public:
+		Timer(const std::string& kName = "Code Block")
+			: m_name(kName), m_start(Clock::now())
+		{
+		}
+		
+		~Timer()
+		{
+			auto end = Clock::now();
+			auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end - m_start);
+			std::cout << name << " took " << duration.count() << " µs" << std::endl;
+		}
+	};
+} // End namespace (AOC)
 
 #endif // !ADVENTOFCODESTD_HPP
